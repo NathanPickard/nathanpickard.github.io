9a4dff2b39b1434b6a58db063caacc07fc084b48
Nathan Pickard <nathan.pickard87@gmail.com>
2017-06-15 20:51:33 -0700

    Enhancements

--- components/contactFormController.js
+++ components/contactFormController.js
@@ -1,57 +1,58 @@
-(function(){
+(function () {
   'use strict';
 
   angular.module('Portfolio.controllers')
-  .controller('contactFormController', ['$scope', '$http', '$mdToast', '$animate',
+    .controller('contactFormController', ['$scope', '$http', '$mdToast', '$animate',
 
-    function($scope, $http, $mdToast, $animate) {
-      $scope.toastPosition = {
-        bottom: false,
-        top: true,
-        left: false,
-        right: true
-      };
-      $scope.getToastPosition = function () {
-        return Object.keys($scope.toastPosition)
-          .filter(function (pos) {
-            return $scope.toastPosition[pos];
-          })
-          .join(' ');
-      };
+      function ($scope, $http, $mdToast, $animate) {
+        $scope.toastPosition = {
+          bottom: false,
+          top: true,
+          left: false,
+          right: true
+        };
 
-      this.sendMail = function () {
-        $mdToast.show(
-          $mdToast.simple()
-          .content('Thanks for your message ' + this.contactName + ' You Rock!')
-                .position($scope.getToastPosition())
-                .hideDelay(5000)
-        );
+        $scope.getToastPosition = function () {
+          return Object.keys($scope.toastPosition)
+            .filter(function (pos) {
+              return $scope.toastPosition[pos];
+            })
+            .join(' ');
+        };
+
+        this.sendMail = function () {
+
+          var data = ({
+            contactName: this.contactName,
+            contactEmail: this.contactEmail,
+            contactMsg: this.contactMsg
+          });
+
+          $http.post('/contact-form', data).
+            success(function (data, status, headers, config) {
+
+
+              $mdToast.show(
+                $mdToast.simple()
+                  .content('Thanks for your message ' + this.contactName + '! Cheers!')
+                  .position($scope.getToastPosition())
+                  .hideDelay(5000)
+              );
+
+              // $mdToast.show(
+              //   $mdToast.simple()
+              //     .content('Thanks for your message ' + this.contactName + '! Cheers!')
+              //     .position($scope.getToastPosition())
+              //     .hideDelay(5000)
+              // );
+
+            });
+          error(function (data, status, headers, config) {
+
+          });
+        }
       }
-
-      // this.sendMail = function() {
-
-      //   var data = ({
-      //     contactName : this.contactName,
-      //     contactEmail : this.contactEmail,
-      //     contactMsg : this.contactMsg
-      //   });
-
-      //   $http.post('/contact-form', data).
-      //     success(function(data, status, headers, config) {
-      //       $mdToast.show(
-      //         $mdToast.simple()
-      //           .content('Thanks for your message ' + data.contactName + ' You Rock!')
-      //           .position($scope.getToastPosition())
-      //           .hideDelay(5000)
-      //       );
-
-      //     }).
-      //     error(function(data, status, headers, config) {
-
-      //     });        
-      // };
-    }
-  ]);
+    ]);
 })();
 
 // 'use strict';
