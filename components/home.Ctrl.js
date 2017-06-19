6de0f03c655eff779b7f5a8b0cbb8d944f6da67c
Nathan Pickard <nathan.pickard87@gmail.com>
2017-06-18 23:10:55 -0700

    Back to top button and about image added

--- components/home.Ctrl.js
+++ components/home.Ctrl.js
@@ -46,5 +46,17 @@
           nav.toggleSelectSection(section);
         }
 
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.goToTop').fadeIn();
+            } else {
+                $('.goToTop').fadeOut();
+            }
+        });
+        $('.goToTop').click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 1000);
+            return false;
+        });
+
       }])
 })();
