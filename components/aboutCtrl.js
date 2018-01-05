92ba947b3844aa0990646a88fa9945d5062556d9
Nathan Pickard <nathan.pickard87@gmail.com>
2018-01-04 21:56:44 -0800

    .addTab and removeTab removed

--- components/aboutCtrl.js
+++ components/aboutCtrl.js
@@ -48,13 +48,13 @@
       if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
       if (current + 1) $log.debug('Hello ' + selected.title + '!');
     });
-    $scope.addTab = function (title, view) {
-      view = view || title + " Content View";
-      tabs.push({ title: title, content: view, disabled: false });
-    };
-    $scope.removeTab = function (tab) {
-      var index = tabs.indexOf(tab);
-      tabs.splice(index, 1);
+    // $scope.addTab = function (title, view) {
+    //   view = view || title + " Content View";
+    //   tabs.push({ title: title, content: view, disabled: false });
+    // };
+    // $scope.removeTab = function (tab) {
+    //   var index = tabs.indexOf(tab);
+    //   tabs.splice(index, 1);
     };
   }
 })();
