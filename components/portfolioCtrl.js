d90c0653031ad6038427d24216e3181a9384cfbc
Nathan Pickard <nathan.pickard87@gmail.com>
2018-01-14 19:55:30 -0800

    Image alignment and nav adjusted and centered

--- components/portfolioCtrl.js
+++ components/portfolioCtrl.js
@@ -5,20 +5,19 @@
     .controller('portfolioCtrl', function ($scope) {
 
       $scope.photos = [
-        { src: 'https://s26.postimg.org/84rmb5zo9/ftfss1.png', desc: 'Image 01' },
-        { src: 'https://s26.postimg.org/n0q5iw0u1/ftfss2.png', desc: 'Image 02' },
-        { src: 'https://s26.postimg.org/o30c1gty1/ftfss3.png', desc: 'Image 03' },
-        { src: '', desc: '' },
-        { src: '', desc: '' }
+        { src: 'https://s26.postimg.org/jlgescf55/wdss1.png', desc: 'Image 01' },
+        { src: 'https://s26.postimg.org/6gf5apgll/wdss2.png', desc: 'Image 02' },
+        { src: 'https://s26.postimg.org/i5j4yp2fd/wdss3.png', desc: 'Image 03' },
+        { src: 'https://s26.postimg.org/bru1vgcyx/wdss4.png', desc: 'Image 04' },
+        { src: 'https://s26.postimg.org/3z3e3jc5l/wdss5.png', desc: 'Image 05' }
       ];
 
       $scope.photos.set1 = [
-        { set1: 'https://s26.postimg.org/jlgescf55/wdss1.png', desc: 'Image 04' },
-        { set1: 'https://s26.postimg.org/6gf5apgll/wdss2.png', desc: 'Image 05' },
-        { set1: 'https://s26.postimg.org/i5j4yp2fd/wdss3.png', desc: 'Image 06' },
-        { set1: 'https://s26.postimg.org/bru1vgcyx/wdss4.png', desc: 'Image 07' },
-        { set1: 'https://s26.postimg.org/3z3e3jc5l/wdss5.png', desc: 'Image 08' }
+        { set1: 'https://s26.postimg.org/84rmb5zo9/ftfss1.png', desc: 'Image 01' },
+        { set1: 'https://s26.postimg.org/n0q5iw0u1/ftfss2.png', desc: 'Image 02' },
+        { set1: 'https://s26.postimg.org/o30c1gty1/ftfss3.png', desc: 'Image 03' }
       ];
+
 
       $scope._Index = 0;
 
